97c99f65d98ecc5253240e39c8e6f2994c7531f9
Zubair Khalid <zubairkhalidce@gmail.com>
2023-05-31 13:49:06 +0100

    added types

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+import { BigNumberish, BytesLike } from "ethers";
+
+export type GasFee = {[key: string]: string} | string;
+
+export interface GasEstimate {
+  preVerificationGas: BigNumberish;
+  verificationGas: BigNumberish;
+  callGasLimit: BigNumberish;
+}
+
+export interface IUserOperation {
+  sender: string;
+  nonce: BigNumberish;
+  initCode: BytesLike;
+  callData: BytesLike;
+  callGasLimit: BigNumberish;
+  verificationGasLimit: BigNumberish;
+  preVerificationGas: BigNumberish;
+  maxFeePerGas: BigNumberish;
+  maxPriorityFeePerGas: BigNumberish;
+  paymasterAndData: BytesLike;
+  signature: BytesLike;
+}
