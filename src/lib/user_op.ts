0bd62b68d3a2b8f87c448e04c4bf738fcff9c29a
Zubair Khalid <zubairkhalidce@gmail.com>
2023-06-05 16:24:08 +0100

    updated user operation class

--- src/lib/user_op.ts
+++ src/lib/user_op.ts
@@ -1,67 +1,72 @@
 import { ethers } from "ethers";
-import { IUserOperation } from "../types";
+import { IClassUserOP, IUserOperation } from "../types";
 
-export default class UserOp {
+export default class UserOp implements IClassUserOP {
 
-  private DEFAULT_VERIFICATION_GAS_LIMIT = ethers.BigNumber.from(70000);
-  private DEFAULT_CALL_GAS_LIMIT = ethers.BigNumber.from(35000);
-  private DEFAULT_PRE_VERIFICATION_GAS = ethers.BigNumber.from(21000);
-
-  private userOp: IUserOperation = {
+  private DEFAULT_USER_OP: IUserOperation = {
     sender: ethers.constants.AddressZero,
     nonce: ethers.constants.Zero,
     initCode: ethers.utils.hexlify("0x"),
     callData: ethers.utils.hexlify("0x"),
-    callGasLimit: this.DEFAULT_CALL_GAS_LIMIT,
-    verificationGasLimit: this.DEFAULT_VERIFICATION_GAS_LIMIT,
-    preVerificationGas: this.DEFAULT_PRE_VERIFICATION_GAS,
+    callGasLimit: ethers.BigNumber.from(0),
+    verificationGasLimit: ethers.BigNumber.from(0),
+    preVerificationGas: ethers.BigNumber.from(0),
     maxFeePerGas: ethers.constants.Zero,
     maxPriorityFeePerGas: ethers.constants.Zero,
     paymasterAndData: ethers.utils.hexlify("0x"),
     signature: ethers.utils.hexlify("0x"),
   }
 
-  constructor(){}
+  private userOp: IUserOperation;
 
-  reset = () => {
-
-    this.userOp = {
-      sender: ethers.constants.AddressZero,
-      nonce: ethers.constants.Zero,
-      initCode: ethers.utils.hexlify("0x"),
-      callData: ethers.utils.hexlify("0x"),
-      callGasLimit: this.DEFAULT_CALL_GAS_LIMIT,
-      verificationGasLimit: this.DEFAULT_VERIFICATION_GAS_LIMIT,
-      preVerificationGas: this.DEFAULT_PRE_VERIFICATION_GAS,
-      maxFeePerGas: ethers.constants.Zero,
-      maxPriorityFeePerGas: ethers.constants.Zero,
-      paymasterAndData: ethers.utils.hexlify("0x"),
-      signature: ethers.utils.hexlify("0x"),
-    }
+  constructor(){
+    this.userOp = {...this.DEFAULT_USER_OP};
   }
 
-  copy = (userOp: IUserOperation) => this.userOp = JSON.parse(JSON.stringify(userOp)) as IUserOperation;
+  reset = () => {
+    this.userOp = {...this.DEFAULT_USER_OP};
+  }
 
   setSender = (sender: string) => this.userOp.sender = sender;
   setNonce = (nonce: ethers.BigNumberish) => this.userOp.nonce = ethers.BigNumber.from(nonce);
+  
   setInitCode = (initCode: ethers.BytesLike) => this.userOp.initCode = initCode;
   setCallData = (callData: ethers.BytesLike) => this.userOp.callData = callData;
+  
   setCallGasLimit = (callGasLimit: ethers.BigNumberish) => this.userOp.callGasLimit = ethers.BigNumber.from(callGasLimit);
-  incVerificationGasLimit = (amount: ethers.BigNumberish) => this.userOp.verificationGasLimit = ethers.BigNumber.from(this.userOp.verificationGasLimit).add(ethers.BigNumber.from(amount));
   setVerificationGasLimit = (verificationGasLimit: ethers.BigNumberish) => this.userOp.verificationGasLimit = ethers.BigNumber.from(verificationGasLimit);
   setPreVerificationGas = (preVerificationGas: ethers.BigNumberish) => this.userOp.preVerificationGas = ethers.BigNumber.from(preVerificationGas);
+  
   setMaxFeePerGas = (maxFeePerGas: ethers.BigNumberish) => this.userOp.maxFeePerGas = ethers.BigNumber.from(maxFeePerGas);
   setMaxPriorityFeePerGas = (maxPriorityFeePerGas: ethers.BigNumberish) => this.userOp.maxPriorityFeePerGas = ethers.BigNumber.from(maxPriorityFeePerGas);
+  
   setPaymasterAndData = (paymasterAndData: ethers.BytesLike) => this.userOp.paymasterAndData = paymasterAndData;
   setSignature = (signature: ethers.BytesLike) => this.userOp.signature = signature;
 
-  get = (): IUserOperation => this.userOp;
+  set = (userOp: IUserOperation) => this.userOp = {...userOp};
 
-  toJSON = () => {
+  getSender = (): string => this.userOp.sender;
+  getNonce = (): ethers.BigNumber => ethers.BigNumber.from(this.userOp.nonce);
+
+  getInitCode = (): ethers.BytesLike => this.userOp.initCode;
+  getCallData = (): ethers.BytesLike => this.userOp.callData;
+
+  getCallGasLimit = (): ethers.BigNumber => ethers.BigNumber.from(this.userOp.callGasLimit);
+  getVerificationGasLimit = (): ethers.BigNumber => ethers.BigNumber.from(this.userOp.verificationGasLimit);
+  getPreVerificationGas = (): ethers.BigNumber => ethers.BigNumber.from(this.userOp.preVerificationGas);
+
+  getMaxFeePerGas = (): ethers.BigNumber => ethers.BigNumber.from(this.userOp.maxFeePerGas);
+  getMaxPriorityFeePerGas = (): ethers.BigNumber => ethers.BigNumber.from(this.userOp.maxPriorityFeePerGas);
+
+  getPaymasterAndData = (): ethers.BytesLike => this.userOp.paymasterAndData;
+  getSignature = (): ethers.BytesLike => this.userOp.signature;
+  
+  get = (): IUserOperation => this.userOp;
+  getDefaults = (): IUserOperation => this.DEFAULT_USER_OP;
+
+  toJSON = (): IUserOperation => {
 
     const userOp = JSON.parse(JSON.stringify(this.userOp));
-
-    // console.log(userOp);
 
     userOp.nonce = userOp.nonce.hex;
     userOp.callGasLimit = userOp.callGasLimit.hex;
@@ -70,7 +75,7 @@ export default class UserOp {
     userOp.maxFeePerGas = userOp.maxFeePerGas.hex;
     userOp.maxPriorityFeePerGas = userOp.maxPriorityFeePerGas.hex;
 
-    return userOp;
+    return userOp as IUserOperation;
 
   }
 
