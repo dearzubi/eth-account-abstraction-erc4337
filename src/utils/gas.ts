415c4e2e238878e2f35b67ed18f73ec5dab2e96a
Zubair Khalid <zubairkhalidce@gmail.com>
2023-06-05 16:19:07 +0100

    improved fee calculation logic and changed return types to return Bignumber

--- src/utils/gas.ts
+++ src/utils/gas.ts
@@ -1,51 +1,24 @@
 import { ethers } from "ethers";
-import {GasFee, GasEstimate, IUserOperation} from "../types";
+import { GasFee } from "../types";
 
 export const gasFee = async (
   provider: ethers.providers.JsonRpcProvider, 
   eip1559: boolean = true
 ): Promise<GasFee> => {
 
-    let [gasFee, block] = await Promise.all([
-      provider.send("eth_gasPrice", []),
-      provider.getBlock("latest"),
-    ]);
+  const [fee, block] = await Promise.all<[Promise<ethers.BigNumber>, Promise<ethers.providers.Block>]>
+  ([
+    provider.send("eth_gasPrice", []),
+    provider.getBlock("latest"),
+  ]);
 
-    gasFee = ethers.BigNumber.from(gasFee);
+  if(block?.baseFeePerGas && eip1559){
+    let maxPriorityFee = ethers.BigNumber.from(fee).sub(block.baseFeePerGas);
+    maxPriorityFee = maxPriorityFee.add(maxPriorityFee.div(100).mul(13))
+    const maxFee = block.baseFeePerGas.mul(2).add(maxPriorityFee);
+    return { maxFee: maxFee, maxPriorityFee: maxPriorityFee };
+  }
 
-    if(block?.baseFeePerGas && eip1559){
-      block.baseFeePerGas = ethers.BigNumber.from(block.baseFeePerGas);
-      const maxPriorityFee = gasFee.sub(block.baseFeePerGas);
-      const maxFee = block.baseFeePerGas.mul(2).add(maxPriorityFee);
-      return { maxFee: maxFee.toHexString(), maxPriorityFee: maxPriorityFee.toHexString() };
-    }
-
-    return gasFee.toHexString();
+  return ethers.BigNumber.from(fee);
     
 };
-
-export const estimateWalletCreationGas = async (
-  provider: ethers.providers.JsonRpcProvider,
-  initCode: ethers.BytesLike
-): Promise<string> => {
-  
-  const initCodeHex = ethers.utils.hexlify(initCode);
-  return (await provider.estimateGas({
-    to: initCodeHex.substring(0, 42),
-    data: "0x" + initCodeHex.substring(42),
-  })).toHexString()
-
-};
-
-export const estimateUserOpGas = async (
-  //provider should be connected to a node supporting ERC4337 eth methods
-  erc4337NodeProvider: ethers.providers.JsonRpcProvider,
-  userOp: IUserOperation,
-  entryPoint: string
-): Promise<GasEstimate> => {
-
-  const estimate = (await erc4337NodeProvider.send("eth_estimateUserOperationGas", [userOp, entryPoint]
-  )) as GasEstimate;
-
-  return estimate;
-}
