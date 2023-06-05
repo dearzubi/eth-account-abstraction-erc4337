0c0a2a98e14c724248ad05be13d6ef3515f6343c
Zubair Khalid <zubairkhalidce@gmail.com>
2023-06-05 16:22:26 +0100

    added and improved types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
-import { BigNumberish, BytesLike } from "ethers";
+import { BigNumberish, BytesLike, BigNumber } from "ethers";
 
 export interface IGasFee {
-  maxFee: string;
-  maxPriorityFee: string;
+  maxFee: BigNumber;
+  maxPriorityFee: BigNumber;
 }
 
-export type GasFee = IGasFee | string;
+export type GasFee = IGasFee | BigNumber;
 
 export enum AccountTypes {
   Simple = 'Simple'
@@ -29,4 +29,49 @@ export interface IUserOperation {
   maxPriorityFeePerGas: BigNumberish;
   paymasterAndData: BytesLike;
   signature: BytesLike;
+}
+
+export interface IClassUserOP{
+
+  reset(): void;
+
+  setSender(sender: string): void;
+  setNonce(nonce: BigNumberish): void;
+  setInitCode(initCode: BytesLike): void;
+  setCallData(callData: BytesLike): void;
+  setCallGasLimit(callGasLimit: BigNumberish): void;
+  setVerificationGasLimit(verificationGasLimit: BigNumberish): void;
+  setPreVerificationGas(preVerificationGas: BigNumberish): void;
+  setMaxFeePerGas(maxFeePerGas: BigNumberish): void;
+  setMaxPriorityFeePerGas(maxPriorityFeePerGas: BigNumberish): void;
+  setPaymasterAndData(paymasterAndData: BytesLike): void;
+  setSignature(signature: BytesLike): void;
+
+  set(userOp: IUserOperation): void;
+  
+  getSender(): string;
+  getNonce(): BigNumber;
+  getInitCode(): BytesLike;
+  getCallData(): BytesLike;
+  getCallGasLimit(): BigNumber;
+  getVerificationGasLimit(): BigNumber;
+  getPreVerificationGas(): BigNumber;
+  getMaxFeePerGas(): BigNumber;
+  getMaxPriorityFeePerGas(): BigNumber;
+  getPaymasterAndData(): BytesLike;
+  getSignature(): BytesLike;
+
+  get(): IUserOperation;
+  getDefaults(): IUserOperation;
+
+  toJSON(): IUserOperation;
+
+}
+
+export interface ISenderDepositInfo {
+  deposit: BigNumber;
+  staked: boolean;
+  stake: BigNumber;
+  unstakeDelaySec: BigNumber;
+  withdrawTime: BigNumber;
 }
